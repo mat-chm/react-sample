0a8ad9f199f87cc166c420b75bb488541cbd1253
Mateusz Chmielewski <Mateusz.Chmielewski@endava.com>
2024-07-15 12:44:28 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
   },
 })
